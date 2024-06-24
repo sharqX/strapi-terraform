92bb689b461ab20e54f8af36ec7589d5de04f1c2
sharqX <shariquerahman24@outlook.com>
2024-06-25 03:26:10 +0530

    revert

--- .github/infra/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.strapi-server.public_ip
-}
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-sudo bash -E nodesource_setup.sh
-sudo apt update && sudo apt install nodejs -y
-sudo npm install -g yarn && sudo npm install -g pm2
-sudo pm2 start /my-strapi-project/server.js
-pm2 save && pm2 start
