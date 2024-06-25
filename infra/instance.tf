9654ac1c459a1107f53923761c95685cdda8ae8c
sharqX <shariquerahman24@outlook.com>
2024-06-25 14:50:17 +0530

    changed instance type

--- infra/instance.tf
+++ infra/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "strapi-server" {
   ami           = "ami-05e00961530ae1b55"
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   subnet_id = aws_subnet.sub1.id
   vpc_security_group_ids = [aws_security_group.auto-sg.id]
   associate_public_ip_address = true
